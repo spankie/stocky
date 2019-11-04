a42d5fd165a0d6f716810a7fe9309cf0728e383b
Odohi David <odohi.david@gmail.com>
2019-11-04 03:31:19 +0100

    add lookup stock service

--- src/main/java/spankie/service/StockService.java
+++ src/main/java/spankie/service/StockService.java
@@ -1,19 +1,33 @@
 package spankie.service;
 
 import java.util.List;
+import java.util.Map;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.net.HttpURLConnection;
+// import java.net.MalformedURLException;
+import java.net.URL;
+import java.io.BufferedReader;
+// import java.io.DataOutputStream;
+// import java.io.IOException;
+import java.io.InputStreamReader;
+import org.springframework.http.HttpStatus;
+// import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.http.HttpStatus;
 
 import spankie.repository.StockRepository;
 import spankie.model.Stock;
 import spankie.exception.CustomException;
+// import spankie.dto.StockResponseDTO;
 
 @Service
 public class StockService {
   @Autowired
   private StockRepository stockRepository;
+  @Autowired
+  private UrlService urlService;
 
   public List<Stock> listUsersStock(String username) {
     List<Stock> stocks = stockRepository.findByUsername(username);
@@ -23,11 +37,8 @@ public class StockService {
     return stocks;
   }
 
-  public Stock lookup(String symbol) {
-
-    Stock stock = new Stock();
-    // search the api with the key: 
-    return stock;
-
+  public Map<String, Object> lookup(String symbol) {
+    String url = "https://cloud-sse.iexapis.com/stable/stock/"+symbol+"/quote?token=sk_e6e5f9391b6549a5b1e1129d8ff04026";
+    return urlService.GET(url);
   }
 }
