0be2efe82008ca93a64f6b488537afa10aba103b
Odohi David <odohi.david@gmail.com>
2019-11-04 03:27:50 +0100

    update swagger config

--- src/main/java/spankie/configuration/SwaggerConfig.java
+++ src/main/java/spankie/configuration/SwaggerConfig.java
@@ -35,6 +35,7 @@ public class SwaggerConfig {
         .useDefaultResponseMessages(false)//
         .securitySchemes(new ArrayList<>(Arrays.asList(new ApiKey("Bearer %token", "Authorization", "Header"))))//
         .tags(new Tag("users", "Operations about users"))//
+        .tags(new Tag("stocks", "Operations about stocks"))//
         .tags(new Tag("ping", "Just a ping"))//
         .genericModelSubstitutes(Optional.class);
 
@@ -44,7 +45,13 @@ public class SwaggerConfig {
     return new ApiInfoBuilder()//
         .title("JSON Web Token Authentication API")//
         .description(
-            "This is a sample JWT authentication service. You can find out more about JWT at [https://jwt.io/](https://jwt.io/). For this sample, you can use the `admin` or `client` users (password: admin and client respectively) to test the authorization filters. Once you have successfully logged in and obtained the token, you should click on the right top button `Authorize` and introduce it with the prefix \"Bearer \".")//
+            "This is a sample JWT authentication service. "+
+            "You can find out more about JWT at [https://jwt.io/](https://jwt.io/). "+
+            "For this sample, you can use the `admin` or `client` users (password: admin "+
+            "and client respectively) to test the authorization filters. "+
+            "Once you have successfully logged in and obtained the token, "+
+            "you should click on the right top button `Authorize` and introduce "+
+            "it with the prefix \"Bearer \".")//
         .version("1.0.0")//
         .license("MIT License").licenseUrl("http://opensource.org/licenses/MIT")//
         .contact(new Contact(null, null, "mauriurraco@gmail.com"))//
