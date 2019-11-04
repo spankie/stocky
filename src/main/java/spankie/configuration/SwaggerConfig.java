9e1a3796c710b8c7fac348380d44aba6bfc391a5
Odohi David <odohi.david@gmail.com>
2019-11-04 12:43:55 +0100

    add tests

--- src/main/java/spankie/configuration/SwaggerConfig.java
+++ src/main/java/spankie/configuration/SwaggerConfig.java
@@ -43,18 +43,12 @@ public class SwaggerConfig {
 
   private ApiInfo metadata() {
     return new ApiInfoBuilder()//
-        .title("JSON Web Token Authentication API")//
+        .title("Stock management application")//
         .description(
-            "This is a sample JWT authentication service. "+
-            "You can find out more about JWT at [https://jwt.io/](https://jwt.io/). "+
-            "For this sample, you can use the `admin` or `client` users (password: admin "+
-            "and client respectively) to test the authorization filters. "+
-            "Once you have successfully logged in and obtained the token, "+
-            "you should click on the right top button `Authorize` and introduce "+
-            "it with the prefix \"Bearer \".")//
+            "This is a simple stock management application")//
         .version("1.0.0")//
         .license("MIT License").licenseUrl("http://opensource.org/licenses/MIT")//
-        .contact(new Contact(null, null, "mauriurraco@gmail.com"))//
+        .contact(new Contact(null, null, "odohi.david@gmail.com"))//
         .build();
   }
 
---
+++ src/main/java/test/UserTest.java
@@ -0,0 +1,15 @@
+package test;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class EmployeeRepositoryIntegrationTest {
+ 
+    @Autowired
+    private TestEntityManager entityManager;
+ 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+ 
+    // write test cases here
+ 
+}
